0b48e720422d3e119db6704c7a712953fa86dec7
hanseltertius <152313201+hanseltertius@users.noreply.github.com>
2025-05-26 21:16:00 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,3 +1,12 @@
+from transformers import pipeline
 import streamlit as st
+
 st.title("aplikasi sederhana")
 st.write("Ini adalah aplikasi sederhana")
+
+classifier = pipeline("sentiment-analysis")
+
+text_input = st.text_input("Insert a sentence")
+if st.button("Submit"):
+    result = classifier(text_input)
+    st.json(result)
